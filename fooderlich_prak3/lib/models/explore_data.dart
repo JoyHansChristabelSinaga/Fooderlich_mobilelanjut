5f3a9c28fb61e043d5a5a32cfa8522127c7321d5
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2022-10-04 09:05:14 +0700

    Perbaikan file dengan file file backup.

--- fooderlich_prak3/lib/models/explore_data.dart
+++ fooderlich_prak3/lib/models/explore_data.dart
@@ -1,5 +1,7 @@
 import 'models.dart';
+
 class ExploreData {
- final List<ExploreRecipe> todayRecipes;
- final List<Post> friendPosts;
- ExploreData(this.todayRecipes, this.friendPosts);
+  final List<ExploreRecipe> todayRecipes;
+  final List<Post> friendPosts;
+  ExploreData(this.todayRecipes, this.friendPosts);
+}
--- fooderlich_prak3/lib/models/instruction.dart
+++ fooderlich_prak3/lib/models/instruction.dart
@@ -1,10 +1,14 @@
 part of 'explore_recipe.dart';
 
 class Instruction {
-  String imageUrl;
-  String description;
-  int durationInMinutes;
-  Instruction({this.imageUrl, this.description, this.durationInMinutes});
+  late String imageUrl;
+  late String description;
+  late int durationInMinutes;
+
+  Instruction(
+      {required this.imageUrl,
+      required this.description,
+      required this.durationInMinutes});
   Instruction.fromJson(Map<String, dynamic> json) {
     imageUrl = json['imageUrl'] ?? '';
     description = json['description'] ?? '';
