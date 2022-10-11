85475cb6653c52d68b97d1e2d9fa0ad72eb540f4
JoyHansChristabelSinaga <83484843+JoyHansChristabelSinaga@users.noreply.github.com>
2022-10-11 08:28:22 +0700

    Update Home, Models, and adding Grocery Screen

--- fooderlich_prak3/lib/home.dart
+++ fooderlich_prak3/lib/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'components/card1.dart';
-import 'components/card2.dart';
-import 'components/card3.dart';
-import 'models/explore_recipe.dart';
+// import 'components/card1.dart';
+// import 'components/card2.dart';
+// import 'components/card3.dart';
+// import 'models/explore_recipe.dart';
 import 'screens/explore_screen.dart';
 import 'screens/recipes_screen.dart';
+import 'screens/grocery_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,74 +18,75 @@ class _HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     ExploreScreen(),
     RecipesScreen(),
-    Card1(
-        recipe: ExploreRecipe(
-            authorName: 'Ray Wenderlich',
-            title: 'The Art of Dough',
-            subtitle: 'Editor\'s Choice',
-            message: 'Learn to make the perfect bread.',
-            backgroundImage: 'assets/magazine_pics/card_bread.jpg',
-            backgroundImageSource: '',
-            calories: '',
-            cardType: '',
-            description: '',
-            dietType: '',
-            durationInMinutes: '',
-            ingredients: [],
-            instructions: [],
-            profileImage: '',
-            role: '',
-            source: '',
-            tags: [])),
-    Card2(
-        recipe: ExploreRecipe(
-            authorName: 'Mike Katz',
-            role: 'Smoothie Connoisseur',
-            profileImage: 'assets/profile_pics/person_katz.jpeg',
-            title: 'Recipe',
-            subtitle: 'Smoothies',
-            backgroundImage: 'assets/magazine_pics/mag2.png',
-            backgroundImageSource: '',
-            calories: '',
-            cardType: '',
-            description: '',
-            dietType: '',
-            durationInMinutes: '',
-            ingredients: [],
-            instructions: [],
-            source: '',
-            tags: [],
-            message: '')),
-    Card3(
-        recipe: ExploreRecipe(
-            title: 'Vegan Trends',
-            tags: [
-              'Healthy',
-              'Vegan',
-              'Carrots',
-              'Greens',
-              'Wheat',
-              'Pescetarian',
-              'Mint',
-              'Lemongrass',
-              'Salad',
-              'Water'
-            ],
-            backgroundImage: 'assets/magazine_pics/mag3.png',
-            backgroundImageSource: '',
-            calories: '',
-            cardType: '',
-            description: '',
-            dietType: '',
-            durationInMinutes: '',
-            ingredients: [],
-            instructions: [],
-            profileImage: '',
-            role: '',
-            source: '',
-            authorName: '',
-            message: '',
-            subtitle: '')),
+    const GroceryScreen(),
+    // Card1(
+    //     recipe: ExploreRecipe(
+    //         authorName: 'Ray Wenderlich',
+    //         title: 'The Art of Dough',
+    //         subtitle: 'Editor\'s Choice',
+    //         message: 'Learn to make the perfect bread.',
+    //         backgroundImage: 'assets/magazine_pics/card_bread.jpg',
+    //         backgroundImageSource: '',
+    //         calories: '',
+    //         cardType: '',
+    //         description: '',
+    //         dietType: '',
+    //         durationInMinutes: '',
+    //         ingredients: [],
+    //         instructions: [],
+    //         profileImage: '',
+    //         role: '',
+    //         source: '',
+    //         tags: [])),
+    // Card2(
+    //     recipe: ExploreRecipe(
+    //         authorName: 'Mike Katz',
+    //         role: 'Smoothie Connoisseur',
+    //         profileImage: 'assets/profile_pics/person_katz.jpeg',
+    //         title: 'Recipe',
+    //         subtitle: 'Smoothies',
+    //         backgroundImage: 'assets/magazine_pics/mag2.png',
+    //         backgroundImageSource: '',
+    //         calories: '',
+    //         cardType: '',
+    //         description: '',
+    //         dietType: '',
+    //         durationInMinutes: '',
+    //         ingredients: [],
+    //         instructions: [],
+    //         source: '',
+    //         tags: [],
+    //         message: '')),
+    // Card3(
+    //     recipe: ExploreRecipe(
+    //         title: 'Vegan Trends',
+    //         tags: [
+    //           'Healthy',
+    //           'Vegan',
+    //           'Carrots',
+    //           'Greens',
+    //           'Wheat',
+    //           'Pescetarian',
+    //           'Mint',
+    //           'Lemongrass',
+    //           'Salad',
+    //           'Water'
+    //         ],
+    //         backgroundImage: 'assets/magazine_pics/mag3.png',
+    //         backgroundImageSource: '',
+    //         calories: '',
+    //         cardType: '',
+    //         description: '',
+    //         dietType: '',
+    //         durationInMinutes: '',
+    //         ingredients: [],
+    //         instructions: [],
+    //         profileImage: '',
+    //         role: '',
+    //         source: '',
+    //         authorName: '',
+    //         message: '',
+    //         subtitle: '')),
   ];
   void _onItemTapped(int index) {
     setState(() {
